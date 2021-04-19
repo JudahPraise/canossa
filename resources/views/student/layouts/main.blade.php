d3bdc12c7c7a3a7bda1919dab559e58986cff99b
jeromelagancia <82696592+jeromelagancia@users.noreply.github.com>
2021-04-19 13:44:16 +0800

    placing navbar in student

--- resources/views/student/layouts/main.blade.php
+++ resources/views/student/layouts/main.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="container-fluid d-flex flex-column align-items-center"  style="width: 100%; background-color: #e9ecef;">
+@include('partials.student_nav')
+</div>
 
 <div class="container-fluid d-flex flex-column align-items-center mt-3"  style="width: 100%;">
     <h1>STUDENT PAGE</h1>
 </div>
+
 
 
 @endsection
