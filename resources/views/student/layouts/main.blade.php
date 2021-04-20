15ce5db1bd60f3b46b55f79c1acb96e3c02a7b10
JudahPraise <judahpraiseocampo@gmail.com>
2021-04-20 09:01:51 +0800

    Added.env.example

--- resources/views/student/layouts/main.blade.php
+++ resources/views/student/layouts/main.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container-fluid d-flex flex-column align-items-center"  style="width: 100%; background-color: #e9ecef;">
-@include('partials.student_nav')
+  @include('partials.student_nav')
 </div>
 
 <div class="container-fluid d-flex flex-column align-items-center mt-3"  style="width: 100%;">
@@ -12,7 +13,5 @@
       @csrf
     </form>
 </div>
-
-
 
 @endsection
