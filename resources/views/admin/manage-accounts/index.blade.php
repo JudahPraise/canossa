9a01efb05a13b52c293dac71b4e0d3dbc6ef813a
JudahPraise <judahpraiseocampo@gmail.com>
2021-09-23 01:50:17 +0800

    Register employee updated

--- resources/views/admin/manage-accounts/index.blade.php
+++ resources/views/admin/manage-accounts/index.blade.php
@@ -17,8 +17,16 @@
     <div class="card">
       <!-- Card header -->
       <div class="card-header border-0 d-flex align-items-center">
-          <div class="col-6 col-md-6 m-0">
+          <div class="col-6 col-md-6 m-0 d-flex align-items-center">
             <h2 class="mb-0">Employees</h2>
+            <div class="input-group input-group-alternative w-25 ml-2">
+              <select class="form-control form-select" aria-label="Default select example">
+                <option selected>Category</option>
+                <option value="1">Resign Employees</option>
+                <option value="2">Partimer</option>
+                <option value="3">Regular Employee</option>
+              </select>
+            </div>
           </div>
           <div class="col-6 col-md-6 d-flex justify-content-sm-end align-items-center p-0">
             <form class="navbar-search-light form-inline mr-2" id="navbar-search-main">
@@ -31,7 +39,7 @@
                 </div>
               </div>
             </form>
-            <a class="btn btn-sm btn-icon btn-success text-white" type="button" data-toggle="modal" data-target="#registerEmployee" id="addEmployee">
+            <a class="btn btn-icon btn-success text-white" type="button" data-toggle="modal" data-target="#registerEmployee" id="addEmployee">
               <span class="btn-inner--icon"><i class="fas fa-plus mr-2"></i></span>Add Employee
             </a>
           </div>
@@ -129,9 +137,7 @@
                 <label for="validationDefault03">Date of birth</label>
                 <input type="date" name="dob" class="form-control" id="validationDefault03" required>
               </div>
-            </div>
-            <div class="form-row">
-              <div class="col-md-6 mb-3">
+              <div class="col-md-4 mb-3">
                 <label for="selectRole">Role</label>
                 <select class="custom-select" name="role" id="selectRole" required>
                   <option selected disabled value="">Choose...</option>
@@ -142,34 +148,48 @@
                   <option>Doctor</option>
                 </select>
               </div>
-              <div class="col-md-6 mb-3">
-                <label for="employeeID">Employee ID</label>
-                <input type="text" name="employee_id" class="form-control text-success" id="employeeID" disabled>
+            </div>
+            <label for="">Category</label>
+            <div class="form-row d-flex mb-5">
+              <div class="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="regular" name="category" class="custom-control-input">
+                <label class="custom-control-label" for="regular" style="font-size: 1.1rem">Regular Employee</label>
+              </div>
+              <div class="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="partime" name="category" class="custom-control-input">
+                <label class="custom-control-label" for="partime" style="font-size: 1.1rem">Partime Employee</label>
               </div>
             </div>
+            <hr>
+            <h3 class="mb-3">Account</h3>
             <div class="form-row d-flex align-items-center">
-              <div class="col-md-6 mb-3">
-                <label for="email">{{ __('Username') }}</label>
-                <input id="email" type="text" class="form-control" name="email" required>
+              <div class="col-md-5 mb-3">
+                <label for="employeeID">Employee ID</label>
+                <input type="text" name="employee_id" class="form-control" style="font-weight: bold; color: black; font-size: 1.2rem" id="employeeID" disabled>
               </div>
-              <div class="col-md-6 mb-3">
+              <div class="col-md-5 mb-3">
                 <label for="password">{{ __('Password') }}</label>
                 <div class="input-group">
-                  <input id="password" type="text" class="form-control text-success" name="password" disabled>
+                  <input id="password" type="text" class="form-control" style="font-weight: bold; color: black; font-size: 1.2rem" name="password" disabled>
                 </div>
               </div>
+              <div class="col-md-2 mb-3">
+                <div id="qrcode"></div> {{-- TODO: QR Code Function --}}
+                <input type="text" name="qr_token" id="qrTokenInput" hidden>
+              </div>
             </div>
-          </form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button class="btn btn-primary" type="submit" value="Submit Form" onclick="document.getElementById('registerForm').submit()">Register</button>
-        </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button class="btn btn-primary" type="submit" value="Submit Form">Register</button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
 </div>
 
+<script src="{{ asset('js/qrcode.js') }}"></script>
 <script>
   function myFunction() {
     var input, filter, table, tr, td, i, txtValue;
@@ -193,10 +213,26 @@
   const generate = document.getElementById('addEmployee');
 
   generate.addEventListener("click", function(){
-    var randomstring = Math.random().toString(36).slice(-8);
-    document.getElementById('password').value = randomstring;
+    var emp_pass = Math.random().toString(36).slice(-8);
+    var qr_token = generateToken() + generateToken();
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+	    text: qr_token,
+	    width: 100,
+	    height: 100,
+	    colorDark : "#000000",
+	    colorLight : "#ffffff",
+	    correctLevel : QRCode.CorrectLevel.H
+    });
+
+    alert(qr_token);
+    document.getElementById('qrTokenInput').value = qr_token;
+    document.getElementById('password').value = emp_pass;
     document.getElementById('employeeID').value = generateId({{ $count }});
   })
+
+  function generateToken(){
+    return  Math.random().toString(36).substr(2);
+  }
 
   function generateId(d) {
 
@@ -214,8 +250,6 @@
     //CADM-20210922-01 - Admin
     
   }
-
- 
 
 </script>
 @endsection
